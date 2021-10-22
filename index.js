52d8b3390246abe1b997763123c8d43ba61317ca
Sebastian Sanhueza <sebastian.sanhueza.t@usach.cl>
2021-10-22 11:24:35 -0300

    formulario funcional

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-// queda hacer el el flowflex en el segundo slice
-
+//Subir la ultima versión de send.php
+//ver el tema de enviar el fomrulario, quede en el video de soy dalto
 //hacer que al deslizar te diga donde ésta, marcando en el menu
