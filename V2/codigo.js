e2392f836c2a02eedbdd799f3c7002a6ae3d64ba
Sebastian Sanhueza <sebastian.sanhueza.t@usach.cl>
2021-12-29 14:55:32 -0300

    slice de precios lista

--- V2/codigo.js
+++ V2/codigo.js
@@ -3,4 +3,11 @@ console.log("si funcion wey");
 /*
 QUEDE EN:
 En hacer los Slice
+la de precios, falta ver la parte de la visual en desktop
+*/
+
+/*
+Corregir:
+Nuestros servicios
+el clip corta las letras, corregir eso
 */
