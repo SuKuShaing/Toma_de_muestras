b5b1fc044e0c41e2483523e2cbff751e052757d4
Sebastian Sanhueza <sebastian.sanhueza.t@usach.cl>
2021-12-30 17:08:30 -0300

    actualizaciÃon de estado

--- V2/codigo.js
+++ V2/codigo.js
@@ -2,10 +2,13 @@ console.log("si funcion wey");
 
 /*
 QUEDE EN:
+- Hacer el footer
+- Hacer la pestaña de infor de los test
+- Modificar los texto
+- Añadir el código para compartir en fb, para el SEO de google y el favicon
+- Añadir los rastreadores de google analitic, console 
 */
 
 /*
 Corregir:
-Nuestros servicios
-el clip corta las letras, corregir eso
 */
