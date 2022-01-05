736537fdf43c93a34616b4518999bce27731137f
Sebastian Sanhueza <sebastian.sanhueza.t@usach.cl>
2022-01-05 16:40:59 -0300

    Versión como el Dani Khapto Zuñiga la diseño

--- V2/codigo.js
+++ V2/codigo.js
@@ -2,10 +2,11 @@ console.log("si funcion wey");
 
 /*
 QUEDE EN:
-- Hacer la pestaña de información de los test
 - Enviar al Mau, Modificar los texto
 - Añadir el código para compartir en fb, para el SEO de google y el favicon
-- Añadir los rastreadores de google analitycs, search console 
+- Añadir los rastreadores de google analitycs, search console
+- Colocar los Rich Snippet
+
 */
 
 /*
